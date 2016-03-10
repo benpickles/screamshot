39ec0617e2939a31c2d63ce0d31a98e4c35e3358
Ben Pickles <spideryoung@gmail.com>
2016-03-10 22:04:56 +0000

    Logo.

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,8 @@ require_relative 'screenshot'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :root, File.expand_path('../../', __FILE__)
+
   post '/screenshot' do
     url = params[:url]
     tmpfile = Screenshot.fetch(url)

b2b9bf25b381612c0ea37c916f1bae1d28e32bce
Ben Pickles <spideryoung@gmail.com>
2016-03-14 09:31:00 +0000

    Ensure the correct MIME type response.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,11 +9,17 @@ RSpec.describe App do
   end
 
   describe 'POST /screenshot' do
+    let(:content_type) { response.headers['Content-Type'] }
     let(:url) { 'http://example.com' }
 
+    before do
+      tmpfile = Tempfile.new(['screenshot', '.png'])
+      allow(Screenshot).to receive(:fetch).with(url).and_return(tmpfile)
+    end
+
     it do
-      expect(Screenshot).to receive(:fetch).with(url)
       post '/screenshot', { url: url }
+      expect(content_type).to eql('image/png')
     end
   end
 end

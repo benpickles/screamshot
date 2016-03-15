257d0cab0ddeb9b020e2d40335c99a58fcaaaed4
Ben Pickles <spideryoung@gmail.com>
2016-03-15 21:21:12 +0000

    Also HTTP cache the homepage.

--- lib/app.rb
+++ lib/app.rb
@@ -2,10 +2,13 @@ require_relative 'screenshot'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  MAX_AGE = 3600
+
   set :root, File.expand_path('../../', __FILE__)
-  set :static_cache_control, [:public, max_age: 3600]
+  set :static_cache_control, [:public, max_age: MAX_AGE]
 
   get '/' do
+    cache_control :public, max_age: MAX_AGE
     send_file File.join(settings.public_folder, 'index.html')
   end
 

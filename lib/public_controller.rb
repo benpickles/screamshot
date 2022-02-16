2b0005930a967325646b99eab018e3a0f0227bc9
Ben Pickles <spideryoung@gmail.com>
2022-02-16 22:15:46 +0000

    Restructure so that the app as a whole is available outside of config.ru (#40)

--- lib/public_controller.rb
+++ lib/public_controller.rb
@@ -1,10 +1,10 @@
 require 'sinatra/base'
 
-class Public < Sinatra::Base
+class PublicController < Sinatra::Base
   MAX_AGE = 3600
 
   set :public_folder, -> { File.join(root, 'docs') }
-  set :root, File.expand_path('../../', __FILE__)
+  set :root, File.expand_path('../', __dir__)
   set :static_cache_control, [:public, max_age: MAX_AGE]
 
   get '/' do
